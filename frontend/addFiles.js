6c6a5733b142aa8e3968db1d9bb2fdb79b734051
Aj Castiglione <antoniocastiglione10@gmail.com>
2019-12-01 18:30:20 -0500

    updated build script and removed add file

--- frontend/addFiles.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-
-fs.copyFile("netlify.toml", "build/netlify.toml", err => {
-  if (err) throw err;
-  console.log("Successfully added netlify.toml to build folder");
-});
